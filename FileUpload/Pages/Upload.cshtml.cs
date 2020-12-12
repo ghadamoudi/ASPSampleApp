83559f2f884d6831917381b89525b94cc1f44f00
ghadamoudi <gaamoudi@kau.edu.sa>
2020-12-12 23:16:27 +0300

    FirstCreate

---
+++ FileUpload/Pages/Upload.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+
+namespace FileUpload.Pages
+{
+    public class UploadModel : PageModel
+    { 
+        private IWebHostEnvironment ihostEnvironment;
+
+        public string FileName { get; set; }
+        
+        public UploadModel(IWebHostEnvironment ihostEnvironment)
+        {
+            this.ihostEnvironment = ihostEnvironment;
+        }
+        public void OnGet()
+        {
+        }
+        public void OnPost(IFormFile afile)
+        {
+            var path = Path.Combine(ihostEnvironment.WebRootPath, "Files", afile.FileName);
+            var stream = new FileStream(path, FileMode.Create);
+            afile.CopyToAsync(stream);
+            FileName = afile.FileName;
+        }   
+    }
+}
